ac7d4ffb91623fd7522da1843223fe822324d209
V.Zinkevichius <joshua.kpi@gmail.com>
2016-12-22 18:29:01 +0200

    Added Counter component. No Redux. Should try redux in next steps

---
+++ src/components/CounterPage/Counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component, PropTypes } from 'react';
+import Button from 'react-bootstrap/lib/Button';
+// import './Counter.css';
+
+const styles = {
+  counterLabel: {
+    display: 'inline-block',
+    marginRight: '20px'
+  }
+};
+
+const propTypes = {
+  onClick: PropTypes.func,
+  value: PropTypes.number
+};
+
+const defaultProps = {
+  onClick: () => {},
+  value: 0
+};
+
+class Counter extends Component {
+  render() {
+    const { onClick, value } = this.props;
+
+    return (
+      <div>
+        <div style={styles.counterLabel}>
+          Value: {value}
+        </div>
+        <Button onClick={onClick}>+</Button>
+      </div>
+    );
+  }
+}
+
+Counter.propTypes = propTypes;
+Counter.defaultProps = defaultProps;
+
+export default Counter;
