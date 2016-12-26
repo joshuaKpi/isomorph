0e37979da8637f049d5f85ea3ea8d56d0d4543d1
V.Zinkevichius <joshua.kpi@gmail.com>
2016-12-26 08:55:46 +0200

    Added redux component

--- src/client.js
+++ src/client.js
@@ -4,11 +4,17 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { browserHistory, Router } from 'react-router';
 import routes from './routes';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
+const store = configureStore();
 
 const component = (
-  <Router history={browserHistory}>
-    {routes}
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      {routes}
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(
